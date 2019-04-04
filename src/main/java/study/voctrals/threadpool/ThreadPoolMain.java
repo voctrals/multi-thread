04e1f97537a74510b7fc17930183500076ccadff
liulei <lei.liu@17zuoye.com>
2019-04-04 18:29:07 +0800

    rename module

---
+++ src/main/java/study/voctrals/threadpool/ThreadPoolMain.java
@@ -0,0 +1,14 @@
+package study.voctrals.threadpool;
+
+/**
+ * @author lei.liu
+ * @since 19-4-4
+ */
+public class ThreadPoolMain {
+
+
+    public static void main(String[] args) {
+        System.out.println("看看这个就好了： https://www.cnblogs.com/voctrals/p/8559305.html");
+    }
+
+}
